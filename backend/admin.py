129a01bfa48d4d4bc7cf81f60b362660d3d294cd
VSatej <v.s.satej1998@gmail.com>
2022-09-27 11:59:58 +0530

    add: django admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Employee
 # Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'first_name', 'email', 'phone_no', 'is_active')
+    list_filter = ("username",)
+    search_fields = ['username']
+
+admin.site.register(Employee, EmployeeAdmin)
